80b4b194f991955439fe22050b81451d2bd48f19
Fenixbvh683 <manidelabk031@gmail.com>
2023-06-04 22:00:05 -0300

    Creacion rama JORGE

--- data/index.js
+++ data/index.js
@@ -1,1 +1,13 @@
 //leer y escribir json
+const {readFileSync, writeFileSync} = require('fs');
+const path = require('path');
+
+const leerJSON = () => JSON.parse(readFileSync(path.join(__dirname, 'clima.json') ,'utf-8'));
+
+const escribirJSON = (productos) =>   writeFileSync(path.join(__dirname,'productos.json'),JSON.stringify(productos,null,3),'utf-8');
+
+
+module.exports = {
+    leerJSON,
+    escribirJSON
+}
